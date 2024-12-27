aa218e91d9e3ac053ce6b35cf4b3f9ac81e04ca1
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2024-12-28 00:38:35 +0600

    some changes

--- src/pages/courseList/CourseList.jsx
+++ src/pages/courseList/CourseList.jsx
@@ -34,26 +34,28 @@ const CourseList = () => {
     <div>
       <div className='display'>
         {
-
-          data?.map((course) => (
-            <div className="course-card">
-              <img src={course.image} alt={course.title} className="course-image" />
-              <div className="course-content">
-                <h2 className="course-title">{course.title}</h2>
-                <p className="course-description">{course.description}</p>
-                <div className="course-details">
-                  <span
-                    className="course-badge"
-                    style={{ backgroundColor: course.badge_color }}
-                  >
-                    {course.badge_text}
-                  </span>
-                  <p className="course-instructor">Instructor: {course.instructor_name}</p>
+          data.length === 0 ? <div>
+            <h1 className='text-center text-4xl'>Please Add Some Course,<span className='text-xl font-semibold text-blue-500'>Go To Home PAGE</span></h1>
+          </div> :
+            data?.map((course) => (
+              <div className="course-card">
+                <img src={course.image} alt={course.title} className="course-image" />
+                <div className="course-content">
+                  <h2 className="course-title">{course.title}</h2>
+                  <p className="course-description">{course.description}</p>
+                  <div className="course-details">
+                    <span
+                      className="course-badge"
+                      style={{ backgroundColor: course.badge_color }}
+                    >
+                      {course.badge_text}
+                    </span>
+                    <p className="course-instructor">Instructor: {course.instructor_name}</p>
+                  </div>
+                  <p className="course-date">Created at: {course.created_at}</p>
                 </div>
-                <p className="course-date">Created at: {course.created_at}</p>
               </div>
-            </div>
-          ))
+            ))
         }
 
       </div>
